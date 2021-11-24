4e270a510455f830a1c45c1fc0dcaaed3cf72337
Sonia Prevost <so.prevost@gmail.com>
2021-11-24 15:50:54 +0100

    Add Siret length validation to Artisan

--- app/models/artisan.rb
+++ app/models/artisan.rb
@@ -1,2 +1,9 @@
 class Artisan < ApplicationRecord
+  validate :check_siret_length
+
+  def check_siret_length
+    unless siret.size == 9 || siret.size == 14
+      errors.add(:name, "Le champs doit comporter 9 chiffres pour un Siren ou 14 chiffres pour un Siret")
+    end
+  end
 end
--- spec/models/artisan_spec.rb
+++ spec/models/artisan_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Artisan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Siret" do
+    it "must be either 9 or 14" do
+      expect { Artisan.create(siret: "123456789")}.to change { Artisan.count }.by(1)
+      expect { Artisan.create(siret: "12345678901234")}.to change { Artisan.count }.by(1)
+      expect { Artisan.create(siret: "123456")}.to change { Artisan.count }.by(0)
+    end
+  end
 end
