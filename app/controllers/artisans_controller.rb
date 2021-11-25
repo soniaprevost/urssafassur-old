fa61fae8bfa49f82d9dab1dbc24daf388002e898
Sonia Prevost <so.prevost@gmail.com>
2021-11-25 14:36:25 +0100

    Create artisan from form

--- app/controllers/artisans_controller.rb
+++ app/controllers/artisans_controller.rb
@@ -1,6 +1,22 @@
 class ArtisansController < ApplicationController
 
   def new
-    artisan = Artisan.new
+    @artisan = Artisan.new
+  end
+
+  def create
+    @artisan = Artisan.create(artisan_params)
+
+    if @artisan.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def artisan_params
+    params.require(:artisan).permit(:siret, :secteur)
   end
 end
--- app/models/artisan.rb
+++ app/models/artisan.rb
@@ -3,7 +3,7 @@ class Artisan < ApplicationRecord
 
   def check_siret_length
     unless siret.size == 9 || siret.size == 14
-      errors.add(:name, "Le champs doit comporter 9 chiffres pour un Siren ou 14 chiffres pour un Siret")
+      errors.add(:siret, "Le champs doit comporter 9 chiffres pour un Siren ou 14 chiffres pour un Siret")
     end
   end
 end
