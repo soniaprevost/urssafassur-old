168bb76e6aef7ccb68aba6ac3fbb4be8a0a145b8
Sonia Prevost <so.prevost@gmail.com>
2021-12-03 15:34:12 +0100

    Check only siret and clean tests

--- app/models/artisan.rb
+++ app/models/artisan.rb
@@ -1,10 +1,5 @@
 class Artisan < ApplicationRecord
   validates :secteur, presence: true
-  validate :check_siret_length
-
-  def check_siret_length
-    unless siret.size == 9 || siret.size == 14
-      errors.add(:siret, "Le champs doit comporter 9 chiffres pour un Siren ou 14 chiffres pour un Siret")
-    end
-  end
+  validates :siret, length: { is: 14,
+                              message: "Le champs doit comporter 14 chiffres pour un Siret" }
 end
--- spec/models/artisan_spec.rb
+++ spec/models/artisan_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Artisan, type: :model do
-  context "Siret" do
-    it "must be either 9 or 14" do
-      expect { Artisan.create(siret: "123456789")}.to change { Artisan.count }.by(1)
-      expect { Artisan.create(siret: "12345678901234")}.to change { Artisan.count }.by(1)
-      expect { Artisan.create(siret: "123456")}.to change { Artisan.count }.by(0)
-    end
-  end
+  it { is_expected.to validate_length_of(:siret).is_equal_to(14)
+                                                .with_message("Le champs doit comporter 14 chiffres pour un Siret") }
 end
